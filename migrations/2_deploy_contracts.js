44c2aaa8890c022c1117819d89b2b5eb768f6fb4
Martin Krung <c@martinkrung.org>
2019-05-22 17:23:02 +0200

    make starttime instant on depoly

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,10 +3,10 @@ const utility = require('../helpers/util');
 const JUNE_1ST_UNIX_TIME = 1559347200;
 
 module.exports = async function(deployer, network, accounts) {
-  if (network === 'ropsten') {
+  if (network === 'main') {
+    await deployer.deploy(Lockdrop, JUNE_1ST_UNIX_TIME);
+  } else {
     let time = await utility.getCurrentTimestamp(web3);
     await deployer.deploy(Lockdrop, time);
-  } else {
-    await deployer.deploy(Lockdrop, JUNE_1ST_UNIX_TIME);
   }
 };
